0b974bacfefe1a762a6a35b4e34343e6fd1bbbcc
Tanvir Ahmed <tanvirahmedonly@gmail.com>
2024-03-10 20:06:01 +0600

    Added a little animation in the banner section.

---
+++ js/components/wordflick.js
@@ -0,0 +1,48 @@
+// text animation start
+var words = ['HIGH QUALITY HEAVY EQUIPMENT SPARE PARTS SUPPLIER', 'large availability of genuine and aftermarket spare parts', 'a wide range of quality Ground Engaging Tools for a variety of equipment manufactures'],
+    part,
+    i = 0,
+    offset = 0,
+    len = words.length,
+    forwards = true,
+    skip_count = 0,
+    skip_delay = 20,
+    speed = 70;
+var wordflick = function () {
+  setInterval(function () {
+    if (forwards) {
+      if (offset >= words[i].length) {
+        ++skip_count;
+        if (skip_count == skip_delay) {
+          forwards = false;
+          skip_count = 0;
+        }
+      }
+    }
+    else {
+      if (offset == 0) {
+        forwards = true;
+        i++;
+        offset = 0;
+        if (i >= len) {
+          i = 0;
+        }
+      }
+    }
+    part = words[i].substr(0, offset);
+    if (skip_count == 0) {
+      if (forwards) {
+        offset++;
+      }
+      else {
+        offset--;
+      }
+    }
+    $('.bl_text_animation').text(part);
+  },speed);
+};
+
+$(document).ready(function () {
+  wordflick();
+});
+// text animation end
